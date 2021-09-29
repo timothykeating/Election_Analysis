864ade14a9de50a89678f2136911cd9a970208a0
Timothy Keating <timothykeating@gmail.com>
2021-09-29 14:27:16 -0400

    updated PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -19,19 +19,70 @@ elec_data='Resources_Election_Analysis/election_results.csv'
 # create a new file to write to
 elec_outcome='Resources_Election_Analysis\election_outcome.txt'
 
+# initialize the total_voter_counter variable BEFORE opening the file - of type=integer
+# & initialize the candidates list variable - list
+# && initialize the candidates votes variable - dictionary
+total_votes=0
+cand_list=[]
+cand_votes={}
+
 #open the file that we're READING first
-with open(elec_data) as election_results:
+with open(elec_data) as election_data:
     
-    #read and analyze the data here
+    ###read and analyze the data here
+
     #1. read the file with the reader function in the csv module
-    file_reader=csv.reader(election_results)
-    #1a. print the entire file as a test that its reading it correctly
+    file_reader=csv.reader(election_data)
+    #1z. print the entire file as a test that its reading it correctly
     # for row in file_reader:
     #     print(row)
     
-    #2. print the header row only
+    #2. read the header row
     headers=next(file_reader)
-    print(headers)
+    
+    #3. loop through the csv file to find the total_votes
+    for row in file_reader:
+        total_votes+=1
+#3z. print the vote total to check
+# print(f'the total number of votes is: {total_votes}')
+
+        #4. (continue to) loop through the csv file to find all candidate names
+        cand_name=row[2]
+        #4aa. use an if statement to determine uniqueness
+        if cand_name not in cand_list:
+            #4a. add the candidate name to the list
+            cand_list.append(cand_name)
+#4z. print the candidate list to check
+# print(cand_list)
+
+            #5. (continue to) loop through the csv file & stay in the IF statement...
+            #  begin to tally vote count
+            cand_votes[cand_name]=0
+        #5a. add votes to the vote count for each candidate BUT move out of the if statement...
+        # because the if statement was setting the list of all candidates, and setting each one of their counters to zero
+        # now we want to have it do a plus+1 every time we see the candidate name, which is not logically part of the if statement
+        cand_votes[cand_name]+=1
+#5z. print the candidate vote dictionary to check
+# print(cand_votes)
+
+#6. determine percentage of total votes each candidate got
+# we're over here, flush with the left margin, because we're done with the for-loop
+# AND we're done reading the csv file, completely
+###
+# loop through the dictionary, and declare votes and vote_pct with formulae
+for cand_name in cand_votes:
+    votes=cand_votes[cand_name]
+    vote_pct=float(votes)/float(total_votes)*100
+    print(f'{cand_name}: received {vote_pct:.2f}% of the vote.')
+
+
+
+
+
+
+
+
+
 
 
 
