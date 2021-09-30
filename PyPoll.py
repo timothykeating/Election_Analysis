f8a0065a0089977352696eede1f51641fc90d3cb
Timothy Keating <timothykeating@gmail.com>
2021-09-30 15:47:22 -0400

    election_results.csv

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,7 @@
 import csv
 import os
 
-# Open the data file that we'll be reading from
+# Name the data file that we'll be reading from
 elec_data='Resources_Election_Analysis/election_results.csv'
 # create a new file to write to
 elec_outcome='Resources_Election_Analysis\election_outcome.txt'
@@ -13,13 +13,18 @@ elec_outcome='Resources_Election_Analysis\election_outcome.txt'
 # & initialize the candidates list variable - list
 # && initialize the candidates votes variable - dictionary
 # &&& winning variables - candidate, votes, vote_pct
+# &&&& CHALLENGE VARIABLES 
 total_votes=0
 cand_list=[]
 cand_votes={}
 win_cand=''
 win_votes=0
 win_pct=0
-print('-------------------')
+county_list=[]
+county_votes={}
+most_votes_county=0
+big_county=''
+
 #open the file that we're READING first
 with open(elec_data) as election_data:
     
@@ -58,19 +63,48 @@ with open(elec_data) as election_data:
         cand_votes[cand_name]+=1
 #5z. print the candidate vote dictionary to check
 # print(cand_votes)
+#############CHALLENGE WORK STARTS HERE - COUNTY INFORMATION
+#####chal_1. VOTER TURNOUT FOR EACH COUNTY
+        county_name=row[1]
+        if county_name not in county_list:
+            county_list.append(county_name)
+            county_votes[county_name]=0
+        county_votes[county_name]+=1
 
 #8.  write results to a .txt file, include a header "Election Results" and total vote count above the candidates info
-# we're jumping in @#8 out of order here, because of the way the Module was laid out - sorry bud!
+# we're jumping in @#8 out of order here, because of the way the Module was laid out...
 with open(elec_outcome,'w') as resfile:
     election_results=(
         f'\nElection Results\n'
         f'-------------------\n'
         f'Total Votes: {total_votes:,}\n'
         f'-------------------\n'
+        f'\nCounty Votes:\n'
     )
     print(election_results)
     resfile.write(election_results)
+#####chal_2. % OF TOTAL_VOTES FROM EACH COUNTY
+    for county_name in county_votes:
+        cvotes=county_votes[county_name]
+        cpct=float(cvotes)/float(total_votes)*100
+        each_county=(f'{county_name}: {cpct:,.1f}% ({cvotes:,})\n')
+        print(each_county)
+        resfile.write(each_county)
+#####chal_3. HIGHEST COUNTY TURNOUT
+        if cvotes>most_votes_county:
+            most_votes_county=cvotes
+            big_county=county_name
+    county_win_blurb=(
+        f'\n-------------------\n'
+        f'Largest County Turnout: {big_county}\n'
+        f'-------------------\n'
+    )
+    print(county_win_blurb)
+    resfile.write(county_win_blurb)
 
+
+#####chal_4. PRINT RESULTS IN TERMINAL
+#####chal_5. WRITE RESULTS TO TXT FILE
     #6. determine percentage of total votes each candidate got
     # we're over here, flush with the left margin, because we're done with the for-loop
     # AND we're done reading the csv file, completely
@@ -80,7 +114,7 @@ with open(elec_outcome,'w') as resfile:
         votes=cand_votes[cand_name]
         vote_pct=float(votes)/float(total_votes)*100
         # 6aa. store a line of text for each candidate as each_cand, print & write to the txt file
-        each_cand=(f'{cand_name}: received {vote_pct:.2f}% of the vote.\n')
+        each_cand=(f'\n{cand_name}: received {vote_pct:.1f}%  ({votes:,})\n')
         print(each_cand)
         resfile.write(each_cand)
         ###
@@ -96,7 +130,7 @@ with open(elec_outcome,'w') as resfile:
     # print(f'{win_pct:.2f}%')
     #7.  print winning candidate with f string & all winning variables & write to txt file
     win_summary=(
-        f'-------------------\n'
+        f'\n-------------------\n'
         f'Winner: {win_cand}\n'
         f'Winning Vote Count: {win_votes:,}\n'
         f'Winning Percentage: {win_pct:.2f}%\n'
--- README.md
+++ README.md
@@ -1,23 +1,6 @@
 # Election_Analysis
 Python Module 3
 
-# PyPoll.py File
-The PyPoll.py file was created as I progressed through Module 3.  In order to analyze the data and write the results of the election analysis to a text file, I took many steps, and here they are laid out in order:  
-- Add dependent modules csv and os;
-- Name the files that we'll be reading from and writing to;
-- Initialize all variables to be used later;
-- Open the file that we're reading from;
-- Read the file with csv.reader;
-- Skip the header row data;
-- Loop through the file to:
-	- Find the total number of votes,
-	- Find a list of all candidates,
-	- Find the number of votes for each candidate
-- Open the file that we're writing to;
-- Write on the file:
-	- Header,
-	- Total votes,
-	- Votes per candidate,
-	- Percentage votes per candidate,
-- Establish and declare a winner;
-- Create a summary of the winner & write that information to the file.
+## Project Overview
+
+
