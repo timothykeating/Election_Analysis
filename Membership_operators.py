f8b487517dfba8a8383109974d1627b3940113ff
Timothy Keating <timothykeating@gmail.com>
2021-09-30 13:23:15 -0400

    deleting junk files and updated README

--- Membership_operators.py
+++
@@ -1,7 +0,0 @@
-counties=['Arapahoe','Denver','Jefferson']
-# if 'El Paso' in counties:
-#     print('El Paso is in the list of counties')
-# else:
-#     print ('El Paso is NOT in the list of counties')
-if 'Quincy' not in counties:
-    print('NOOOOOO Quincy')
--- Python_practice_2.py
+++
@@ -1,5 +0,0 @@
-temp=int(input("what is the temp?"))
-if temp>80:
-    print("Turn on the AC.")
-else:
-    print("Open the window.")
