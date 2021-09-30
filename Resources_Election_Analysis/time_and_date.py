e6b7ea21d704ec5d2cc6ef05b64e9a866757a3f5
Timothy Keating <timothykeating@gmail.com>
2021-09-30 13:25:15 -0400

    deleting junk files

--- Resources_Election_Analysis/time_and_date.py
+++
@@ -1,9 +0,0 @@
-#import datetime class form the datetime module
-import datetime as dt
-
-# use the now() attribute on the datetime class to get the present time
-now=dt.datetime.now()
-
-#print the present time
-print('the time right now is ', now)
-
